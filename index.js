0467a76a9ad6d26fb49754b0f804c03fe5b9da1f
eunwoo <eunwoo123@gmail.com>
2022-07-22 22:22:08 +0900

    tracked file 수정

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const open = require('open')
 var childProcess = require('child_process'); 
 
 const app = express()
+const port = 3110;
+
+console.log(process.argv[2])
 
 app.get('', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
+    res.sendFile(__dirname + '/public/index1.html')
 })
 app.use(express.static('public'))
 
-app.listen(3110, () => {
-    console.log('html express server at port 3110')
+app.listen(port, () => {
+    console.log(`html express server at port ${port}`)
     // open("http://localhost:3000", { app: ["google chrome"] });
-    childProcess.exec('start chrome "http://localhost:3110"');
-
+    if(process.argv[2] === undefined) {
+        childProcess.exec(`start chrome "http://localhost:${port}"`);
+    }
+    else if(process.argv[2] === 'kiosk'){
+        childProcess.exec(`start chrome "http://localhost:${port}" --user-data-dir="c:\\temp" --kiosk`);
+    }
 })
 
---
+++ public/js/setting.js
@@ -0,0 +1,43 @@
+// 이미지는 png나 jpg로 설정해 주세요
+// 동영상은 mp4로 설정해 주세요
+// 파일 저장 위치: 이미지 파일은 public/img에 저장, 동영상은 public/vid에 저장
+// main에는 이미지만 설정이 가능합니다.
+// popup에는 이미지와 동영상 모두 설정이 가능합니다.
+
+let content = [
+  // page1 에어 케어 시스템
+  {
+    'main':'올인원에어케어.png',  
+    'popup':'When She Leaves by @Per-Olov Kindgren.mp4',
+    'popup-position':['40%', '50%'],
+  },  
+
+  // page2 실내공기 관리
+  {
+    'main':'올인원에어케어.png', 
+    'popup':'올인원에어케어.png', 
+    'popup-position':['40%', '50%'],
+  },
+
+  // page3 Air Wash
+  {
+    'main':'올인원에어케어.png', 
+    'popup':'15초.mp4',
+    'popup-position':['40%', '50%'],
+  }, 
+
+  // page4 쾌적 제어
+  {
+    'main':'시스템케어팝업.png',
+    'popup':'시스템케어팝업.png',
+    'popup-position':['40%', '50%'],
+  }, 
+
+  // page5 시스템 케어
+  {
+    'main':'올인원에어케어.png', 
+    'popup':'15초.mp4',
+    'popup-position':['40%', '50%'],
+  }
+];
+
